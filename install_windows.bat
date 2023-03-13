e6a46972fb4c568d5015edccfc1df7741293a5af
Khim Synat <synat.khm@gmail.com>
2023-03-13 11:50:24 +0700

    Change .venv for window

--- install_windows.bat
+++ install_windows.bat
@@ -1,3 +1,5 @@
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+python -m pip install virtualenv
+python -m virtualenv .venv
 .\.venv\Scripts\activate      
 python -m pip install -r .\requirements.txt
