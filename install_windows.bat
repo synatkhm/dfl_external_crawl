8d4b81a866700d26c69714be7f6233454d4332ae
Khim Synat <synat.khm@gmail.com>
2023-03-13 14:36:02 +0700

    Change .venv for window

--- install_windows.bat
+++ install_windows.bat
@@ -1,5 +1,4 @@
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
 python -m pip install virtualenv
-python -m virtualenv .venv
-.\.venv\Scripts\activate      
+python -m virtualenv .venv 
 python -m pip install -r .\requirements.txt
--- start_windows.bat
+++ start_windows.bat
@@ -1,3 +1,3 @@
-.\.venv\Scripts\activate 
+
 uvicorn main:app --port 2023
 start http://127.0.0.1:2023
